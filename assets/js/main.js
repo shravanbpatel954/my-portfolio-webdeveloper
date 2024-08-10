512d5a23c423c6119058718fae00273e06be5029
Shravan.B.Patel <shravan.b.patel954@gmail.com>
2024-08-10 17:02:50 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,7 +48,7 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 2000,
     delay: 200,
-//     reset: true
+    reset: true
 });
 
 sr.reveal('.home__data, .about__img,.education__img,.education__subtitle, .skills__subtitle, .skills__text',{}); 
